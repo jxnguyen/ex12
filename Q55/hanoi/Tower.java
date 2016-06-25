9b601d840522b9bfcd2a41f06a0a9447722aa598
jxnguyen <johnxuannguyen@gmail.com>
2016-06-25 12:34:57 +0200

    boiler code

---
+++ Q55/hanoi/Tower.java
@@ -0,0 +1,35 @@
+package hanoi;
+
+public class Tower {
+
+	public Tower(int[] startTower) throws TurmException {
+
+	}
+
+	// MOVE | move the top disc from start tower to end tower, with following
+	// 				conditions: 1) start, end are 'a,b,c' & start != end. 2) start
+	// 				is not empty. 3) top disc on start is <= top disc on end, or
+	// 				end is empty. Returns number representing moved disc.
+	//
+	public final int move(char start, char end) throws TurmException {
+
+	}
+
+	// FINISHED | return true if towers 'a' & 'b' are empty, else false
+	//
+	public final boolean finished() {
+
+	}
+
+	//  START TOWER | produce a copy of the start tower
+	//
+	public int[] startTower() {
+
+	}
+
+	// MOVES | return the number of previous moves
+	//
+	public final int moves() {
+
+	}
+}
