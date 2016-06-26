bf9fef5f1bc9ca98af6648532d7d93666a6a18c2
jxnguyen <johnxuannguyen@gmail.com>
2016-06-26 20:24:11 +0200

    clean up

--- Q55/hanoi/Tower.java
+++ Q55/hanoi/Tower.java
@@ -6,28 +6,28 @@ import java.util.Stack;
 
 public class Tower {
 
-	public HashMap towers;
+	public HashMap<Character, Stack<Integer>> towers;
 
 	public Tower(int[] startTower) throws TurmException {
 		// init start tower
-		Stack start = new Stack();
+		Stack<Integer> start = new Stack<Integer>();
 		for (int x: startTower) start.push(x);
 		// init towers
-		towers = new HashMap(3);
+		towers = new HashMap<Character, Stack<Integer>>(3);
 		towers.put('a', start);
-		towers.put('b', new Stack());
-		towers.put('c', new Stack());
+		towers.put('b', new Stack<Integer>());
+		towers.put('c', new Stack<Integer>());
 	}
 
-	// MOVE | move the top disc from start tower to end tower, with following
+	// MOVE | Move the top disc from start tower to end tower, with following
 	// 				conditions: 1) start, end are 'a,b,c' & start != end. 2) start
 	// 				is not empty. 3) top disc on start is <= top disc on end, or
 	// 				end is empty. Returns number representing moved disc.
 	//
 	public final int move(char start, char end) throws TurmException {
 
-		Stack startTower = (Stack)towers.get(start);
-		Stack endTower = (Stack)towers.get(end);
+		Stack<Integer> startTower = towers.get(start);
+		Stack<Integer> endTower   = towers.get(end);
 
 		if (!("abc".contains(""+start)) || !("abc".contains(""+end))) {
 			throw new TurmException("start/end must be 'a', 'b' or 'c'.");
@@ -38,38 +38,40 @@ public class Tower {
 		else if (startTower.empty()) {
 			throw new TurmException("start tower is empty.");
 		}
-		else if (!endTower.empty() && (int)startTower.peek() > (int)endTower.peek()) {
+		else if (!endTower.empty() && startTower.peek() > endTower.peek()) {
 			throw new TurmException("Cannot move disc onto smaller disc.");
 		}
 		else {
-			int top = (int)startTower.pop();
+			int top = startTower.pop();
 			endTower.push(top);
 			return top;
 		}
 	}
 
-	// FINISHED | return true if towers 'a' & 'b' are empty, else false
+	// FINISHED | Return true if towers 'a' & 'b' are empty, else false
 	//
 	public final boolean finished() {
-		return ((Stack)towers.get('a')).empty() && ((Stack)towers.get('b')).empty();
+		return towers.get('a').empty() && towers.get('b').empty();
 	}
 
-	// //  START TOWER | produce a copy of the start tower
+	// //  START TOWER | Produce a copy of the start tower
 	// //
 	// public int[] startTower() {
 	//
 	// }
 	//
-	// // MOVES | return the number of previous moves
+	// // MOVES | Return the number of previous moves
 	// //
 	// public final int moves() {
 	//
 	// }
 
+	// TO STRING | Return string representation of self.
+	//
 	public String toString() {
-		Stack a = (Stack)towers.get('a');
-		Stack b = (Stack)towers.get('b');
-		Stack c = (Stack)towers.get('c');
+		Stack<Integer> a = towers.get('a');
+		Stack<Integer> b = towers.get('b');
+		Stack<Integer> c = towers.get('c');
 		return a.toString() + "\n" + b.toString() + "\n" + c.toString();
 	}
 
