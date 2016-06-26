e1c9a376b6847d01bc40a1d5169d1e5596b99f6a
jxnguyen <johnxuannguyen@gmail.com>
2016-06-26 22:32:26 +0200

    implemented solve()

--- Q55/Q55.java
+++ Q55/Q55.java
@@ -5,22 +5,36 @@ class Q55 {
 	public static void main(String[] args) {
 
 		try {
-			int[] nums = {1,2,3};
+			int[] nums = {1,2,3,4,5};
 			Tower t = new Tower(nums);
 			System.out.println(t);
 
-			// int[] start = t.startTower();
-			// for (int x : start) System.out.println(x);
-
-			t.move('a', 'c');
-			System.out.println(t);
-			t.move('a', 'b');
-			System.out.println(t);
-
-			System.out.println("Number of moves: " + t.moves());
+			solve(t);
 		}
 		catch (TurmException e) {
 			System.out.println(e);
 		}
+	}
+
+	static void solve(Tower t) throws TurmException {
+
+			while (!t.finished()) {
+				// if even number of discs
+				if (t.discs % 2 == 0) {
+					t.anyMove('a','b');
+					if (t.finished()) break;
+					t.anyMove('a','c');
+					if (t.finished()) break;
+					t.anyMove('b','c');
+				}
+				else {
+					t.anyMove('a','c');
+					if (t.finished()) break;
+					t.anyMove('a','b');
+					if (t.finished()) break;
+					t.anyMove('c','b');
+				}
+			}
+			System.out.println("Solved in " + t.moves() + " moves!");
 	}
 }
