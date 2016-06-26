86b1dbac99316c2f7033da57a6af31f7eb9a87f6
jxnguyen <johnxuannguyen@gmail.com>
2016-06-26 21:41:36 +0200

    updated test suite

--- Q55/Q55.java
+++ Q55/Q55.java
@@ -7,10 +7,14 @@ class Q55 {
 		try {
 			int[] nums = {3,2,1};
 			Tower t = new Tower(nums);
-			int result = t.move('a', 'c');
 			System.out.println(t);
-			System.out.println(t.finished());
 
+			t.move('a', 'c');
+			System.out.println(t);
+			t.move('a', 'b');
+			System.out.println(t);
+
+			System.out.println("Number of moves: " +t.moves());
 		}
 		catch (TurmException e) {
 			System.out.println(e);
